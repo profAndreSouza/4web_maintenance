09380973974ca8d1789e8fc7de31455b091881db
profAndreSouza <andre.souza@sp.senai.br>
2024-07-31 22:44:18 -0300

    datatable and chart components

---
+++ frontend/src/app/components/datatable.tsx
@@ -0,0 +1,160 @@
+import { LuTrash } from "react-icons/lu";
+import { MdEditNote } from "react-icons/md";
+
+export function DataTable() {
+    return (
+        <table className="w-full divide-y divide-gray-300 data-table">
+            <thead>
+                <tr>
+                    <th>Ambiente</th>
+                    <th>Equipamento</th>
+                    <th>Solicitado em</th>
+                    <th>Atendido em</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+                <tr>
+                    <td>Sala 04</td>
+                    <td>Projetor</td>
+                    <td>31/07/24</td>
+                    <td>-</td>
+                    <td className="flex gap-4"><MdEditNote /> <LuTrash /> </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,12 @@ import { MdForklift } from "react-icons/md";
 import { Footer } from "./components/footer";
 import { Aside } from "./components/aside";
 import { Card } from "./components/card";
+import { DataTable as MyDataTable } from "./components/datatable";
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { MainChart } from "./components/mainchart";
+        
+
 
 export default function Home() {
 
@@ -15,10 +21,16 @@ export default function Home() {
     {color: "bg-red-200", qty:"100", text:"O.S. Abertas", icon:<GrNotes size={48} />},
     {color: "bg-green-200", qty:"100", text:"O.S. Concluídas", icon:<IoMdCheckboxOutline size={48} />},
   ];
+
+  const requisitions = [
+    {place: "Sala 01", equipment:"Projetor", requested:"27-jul", completed:"-"},
+    {place: "Sala 01", equipment:"Lousa", requested:"27-jul", completed:"27-jul"},
+    {place: "Sala 02", equipment:"Mesa", requested:"28-jul", completed:"-"},
+  ];
     
   return (
     <div className="h-screen flex flex-col">
-      <div className="flex-1 flex">
+      <div className="flex-1 flex h-4/5">
         
         <Aside />
 
@@ -28,9 +40,9 @@ export default function Home() {
            bg-white/40 p-6 text-center">
             Sistema de Gestão de Manutenção</h1>
 
-          <div>
+          <div className="max-h-svh overflow-y-auto">
+
             <div className="grid grid-cols-4 gap-4 p-6">
-              
               {cards.map((props) => (
                 <Card color={props.color}
                     qty={props.qty}
@@ -38,8 +50,27 @@ export default function Home() {
                     icon={props.icon}
                 />
               ))}
-         
             </div>
+
+            
+            <div className="flex gap-2 p-6 pt-0">
+
+            <div className="p-0  rounded-xl w-1/3">
+              <MainChart />
+            </div>
+            <div className="m-6 mt-0 p-6 bg-white/40 rounded-xl flex-1">
+              {/* <DataTable /> */}
+              <DataTable value={requisitions} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}>
+                  <Column field="place" header="Ambiente" style={{ width: '25%' }}></Column>
+                  <Column field="equipment" header="Equipamento" style={{ width: '25%' }}></Column>
+                  <Column field="requested" header="Solicitado em" style={{ width: '25%' }}></Column>
+                  <Column field="completed" header="Atendido em" style={{ width: '25%' }}></Column>
+              </DataTable>
+
+            </div>
+
+            </div>
+
           </div>
 
 
