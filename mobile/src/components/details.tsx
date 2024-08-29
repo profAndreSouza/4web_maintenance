1e68797de4f072e67bfbaa4eeb8d0d849e6a6687
profAndreSouza <andre.souza@sp.senai.br>
2024-08-29 20:53:49 -0300

    updated

--- mobile/src/components/details.tsx
+++ mobile/src/components/details.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export function DetailsScreen({ route }: { route: any }) {
     const {name, email } = route.params;
     return (
-      <View>
+      <View className="p-5">
         <Text className="text-xl mb-2">Name:</Text>
         <Text className="text-lg mb-5">{name}</Text>
   
