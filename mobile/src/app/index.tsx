9aa0daedf69b0c85da8dd85fc79b34bad2f797b9
profAndreSouza <andre.souza@sp.senai.br>
2024-08-29 20:06:50 -0300

    perninha

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,15 +1,46 @@
 import { Button, Text, TextInput, View } from "react-native";
+import { createStackNavigator } from '@react-navigation/stack';
 import { styled } from 'nativewind';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: {name: string; email: string};
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+function HomeScreen({ navigation }: { navigation: any }) {
+  return (
+    <View className="flex-1 p-5 justify-center">
+      <Text className="text-xl mb-2">Name:</Text>
+      <TextInput
+        className="border border-gray-300 p-3 mb-5 rounded"
+        placeholder="Enter your name"
+      />
+      <Text className="text-xl mb-2">Email</Text>
+      <TextInput
+        className="border border-gray-300 p-3 mb-5 rounded"
+        placeholder="Enter your email"
+      />
+      <Button
+        title="Submit"
+      />
+    </View>
+  )
+}
+
 
 export default function Index() {
   return (
     <View className="flex-1 p-5 justify-center">
-      <Text>Name:</Text>
+      <Text className="text-xl mb-2">Name:</Text>
       <TextInput
-        placeholder="Enter your name"      
+        className="border border-gray-300 p-3 mb-5 rounded"
+        placeholder="Enter your name"
       />
-      <Text>Email</Text>
-      <TextInput 
+      <Text className="text-xl mb-2">Email</Text>
+      <TextInput
+        className="border border-gray-300 p-3 mb-5 rounded"
         placeholder="Enter your email"
       />
       <Button
