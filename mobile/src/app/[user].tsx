12ce1cbb2491bffbbcc84f74a0cd1ad32bd87fcb
profAndreSouza <andre.souza@sp.senai.br>
2024-08-29 19:16:57 -0300

    mobile

---
+++ mobile/src/app/[user].tsx
@@ -0,0 +1,9 @@
+import { Text } from 'react-native';
+
+export default function Home() {
+    return (
+        <Text>Página Usuário</Text>
+    );
+}
+
+// localhost:8081/misael
---
+++ mobile/src/app/home.tsx
@@ -0,0 +1,9 @@
+import { Text } from 'react-native';
+
+export default function Home() {
+    return (
+        <Text>Página Home</Text>
+    );
+}
+
+// localhost:8081/home
