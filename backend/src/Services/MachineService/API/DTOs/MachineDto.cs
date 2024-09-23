9ca91ddae8c35b1840cd18ac41048c1f4a1967f5
profAndreSouza <andre.souza@sp.senai.br>
2024-09-23 14:16:26 -0300

    Updated

--- backend/src/Services/MachineService/API/DTOs/MachineDto.cs
+++
@@ -1,9 +0,0 @@
-namespace MachineService.Application.DTOs
-{
-    public class MachineDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Status { get; set; }
-    }
-}
--- backend/src/Services/MachineService/Domain/Entities/Machine.cs
+++
@@ -1,9 +0,0 @@
-namespace MachineService.Domain.Entities
-{
-    public class Machine
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Status { get; set; }
-    }
-}
